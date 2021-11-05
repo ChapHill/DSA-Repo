9cb71b3d4496d0ae81b965580a282476ac43e968
clay <claycarroll7@gmail.com>
2021-11-05 15:55:22 -0500

    implementation of stack using linked list complete

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -1,5 +1,7 @@
 package stack;
 
+import java.util.NoSuchElementException;
+
 public class Stack<T> {
     private static class Node<T> {
         private T data;
@@ -17,8 +19,52 @@ public class Stack<T> {
     }
 
     private Node<T> top;
+    private int size;
 
     public boolean isEmpty() {
         return top == null;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public void push(T data) {
+        ++size;
+        if(top == null) {
+            this.top = new Node<T>(data, null);
+        } else {
+            Node<T> oldTop = top;
+            this.top = new Node<T>(data, oldTop);
+        }
+    }
+
+    public T pop() {
+        if(isEmpty()) {
+            throw new NoSuchElementException("Empty Stack");
+        } else {
+            Node<T> oldTop = top;
+            T ret = oldTop.data; //return value
+            top = top.next;
+            oldTop = null; //memory cleanup
+            return ret;
+        }
+    }
+
+    public T peek() {
+        return this.top.data;
+    }
+
+    public void printStack() {
+        if(isEmpty()) {
+            throw new NoSuchElementException("Empty Stack");
+        } else {
+            Node<T> runner = top;
+
+            while(runner != null) {
+                System.out.println(runner.data + "\n----------");
+                runner = runner.next;
+            }
+        }
     }
 }
