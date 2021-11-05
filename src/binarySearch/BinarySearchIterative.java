42da1748e9d9318ca6bb3dcb25de7a90b480e262
clay <claycarroll7@gmail.com>
2021-11-05 14:36:57 -0500

    added function for iterative approach to binary search

--- src/binarySearch/BinarySearchIterative.java
+++ src/binarySearch/BinarySearchIterative.java
@@ -15,4 +15,21 @@ package binarySearch;
 
 public class BinarySearchIterative {
 
+    public int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length-1;
+
+        while(left <= right ) {
+            int mid = left + (right-left) / 2;
+            if(target == array[mid]) {
+                return mid;
+            } else if(target < array[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1; // not found
+    }
 }
