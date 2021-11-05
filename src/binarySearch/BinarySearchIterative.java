bfc2dede470944c54741db2d1d6acb0d2ca1a00a
clay <claycarroll7@gmail.com>
2021-11-05 14:31:29 -0500

    Added binary search file

---
+++ src/binarySearch/BinarySearchIterative.java
@@ -0,0 +1,18 @@
+package binarySearch;
+
+/**
+ * Searching algorithm that searches through a SORTED array
+ * continuously divides the array in half until the target is found
+ *
+ * A = array, t = target
+ *
+ * compare t with middle element
+ * if t matches with middle element, return middle index (return type depends on question)
+ * else if t > middle element, cut A in half and search right half
+ * else if t < middle element, cut A in half and search left half
+ * return -1 if not found
+ * **/
+
+public class BinarySearchIterative {
+
+}
